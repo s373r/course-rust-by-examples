02491438b60ef09656adc13f68ee549ea3f093b7
Dima Pristupa <dima.pristupa@pm.me>
2021-10-12 13:54:07 +0300

    🚧 Finish 1.2.1 Debug

--- 1_hello/src/main.rs
+++ 1_hello/src/main.rs
@@ -4,6 +4,7 @@
 // 1. Hello World
 //    1.1 Comments
 //    1.2 Formatted print
+//        1.2.1 Debug
 
 //! Generate library docs for the enclosing item.
 
@@ -102,5 +103,50 @@ fn main() {
 
         let pi = std::f64::consts::PI;
         println!("Pi is roughly {:.3}", pi);
+    }
+
+    //
+    // --- 1.2.1 Debug ---
+    // https://doc.rust-lang.org/rust-by-example/hello/print/print_debug.html
+    //
+    {
+        // This structure cannot be printed either with `fmt::Display` or
+        // with `fmt::Debug`.
+        #[allow(dead_code)]
+        struct UnPrintable(i32);
+
+        // The `derive` attribute automatically creates the implementation
+        // required to make this `struct` printable with `fmt::Debug`.
+        #[derive(Debug)]
+        struct DebugPrintable(i32);
+
+        // println!("{:?}", UnPrintable(1));
+        println!("{:?}", DebugPrintable(2));
+
+        // Derive the `fmt::Debug` implementation for `Structure`. `Structure`
+        // is a structure which contains a single `i32`.
+        #[derive(Debug)]
+        struct Structure(i32);
+
+        // Put a `Structure` inside of the structure `Deep`. Make it printable
+        // also.
+        #[derive(Debug)]
+        struct Deep(Structure);
+
+        // Printing with `{:?}` is similar to with `{}`.
+        println!("{:?} months in a year.", 12);
+        println!(
+            "{1:?} {0:?} is the {actor:?} name.",
+            "Slater",
+            "Christian",
+            actor = "actor's"
+        );
+
+        // `Structure` is printable!
+        println!("Now {:?} will print!", Structure(3));
+
+        // The problem with `derive` is there is no control over how
+        // the results look. What if I want this to just show a `7`?
+        println!("Now {:?} will print!", Deep(Structure(7)));
     }
 }
--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Course link: https://doc.rust-lang.org/stable/rust-by-example
 - [1. Hello World](1_hello/src/main.rs)
   - 1\. 1. Comments
   - 1\. 2. Formatted print
+    - 1\. 2. 1. Debug
 
 
 ## Notes
