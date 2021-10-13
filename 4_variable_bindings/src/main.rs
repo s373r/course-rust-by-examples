fbaf2ad910356723d1c68d63b813841e7fd43c2c
Dima Pristupa <dima.pristupa@pm.me>
2021-10-13 14:54:02 +0300

    🚧 Finish 4. Variable Bindings

--- 4_variable_bindings/src/main.rs
+++ 4_variable_bindings/src/main.rs
@@ -7,5 +7,23 @@ fn main() {
     // https://doc.rust-lang.org/rust-by-example/variable_bindings.html
     println!();
     println!("--- 4. Variable Bindings ---");
-    {}
+    {
+        let an_integer = 1u32;
+        let a_boolean = true;
+        let unit = ();
+
+        // copy `an_integer` into `copied_integer`
+        let copied_integer = an_integer;
+
+        println!("An integer: {:?}", copied_integer);
+        println!("A boolean: {:?}", a_boolean);
+        println!("Meet the unit value: {:?}", unit);
+
+        // The compiler warns about unused variable bindings; these warnings can
+        // be silenced by prefixing the variable name with an underscore
+        let _unused_variable = 3u32;
+
+        // let noisy_unused_variable = 2u32;
+        // DONE: ^ Prefix with an underscore to suppress the warning
+    }
 }
